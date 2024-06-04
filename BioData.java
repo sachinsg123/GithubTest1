9ecb1b3ced5a6bb882dc853a317311bae30f2810
sachin <sachinghule834@gmail.com>
2024-06-04 12:21:01 +0530

    added by sachin developer

---
+++ BioData.java
@@ -0,0 +1,10 @@
+public class BioData {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Biodata features created by sachin branch");
+        System.out.println("new line added in biodata class");
+    }
+    
+}
---
+++ Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+
+
+    public static void main(String[] args) {
+        System.out.println("profile feature added by sachin ");
+    }
+    
+}
